35564283179e484c2cd755446e4b1d1691ba4d03
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-08-19 14:50:22 -0500

    First entry

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to ITSE");
+            Console.WriteLine("Welcome to ITSE1302");
+            Console.Read();
         }
     }
 }
